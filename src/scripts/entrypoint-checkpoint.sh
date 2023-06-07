fb636d090fc300051bfb4085fc53257c36bb06b2
Sébastien Deleuze <sdeleuze@vmware.com>
2023-06-07 12:59:51 +0200

    Leverage spring-projects/spring-framework#30606

--- src/scripts/entrypoint-checkpoint.sh
+++ src/scripts/entrypoint-checkpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 128 > /proc/sys/kernel/ns_last_pid; java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -Dspring.checkpoint.restore=onRefresh -XX:CRaCCheckpointTo=/opt/crac-files -jar /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar
+echo 128 > /proc/sys/kernel/ns_last_pid; java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -Dspring.checkpoint.restore=onRefresh -Dspring.datasource.url=jdbc:postgresql://postgres/cashcard -XX:CRaCCheckpointTo=/opt/crac-files -jar /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar
 sleep infinity
