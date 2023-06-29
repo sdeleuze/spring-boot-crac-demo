6e09e908ea01ca04cf5dc3e2e15486a3d78b6a13
Sébastien Deleuze <sdeleuze@vmware.com>
2023-06-29 17:56:31 +0200

    Add aarch64 support
    Closes gh-2

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM ubuntu:22.04
 
+ARG CRAC_JDK_URL
+
 ENV JAVA_HOME /opt/jdk
 ENV PATH $JAVA_HOME/bin:$PATH
 ENV CRAC_FILES_DIR /opt/crac-files
 
-ADD "https://cdn.azul.com/zulu/bin/zulu17.42.21-ca-crac-jdk17.0.7-linux_x64.tar.gz" $JAVA_HOME/openjdk.tar.gz
+ADD $CRAC_JDK_URL $JAVA_HOME/openjdk.tar.gz
 RUN tar --extract --file $JAVA_HOME/openjdk.tar.gz --directory "$JAVA_HOME" --strip-components 1; rm $JAVA_HOME/openjdk.tar.gz;
 RUN mkdir -p /opt/app
 COPY target/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar
--- checkpoint.sh
+++ checkpoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+case $(uname -m) in
+    arm64)   url="https://cdn.azul.com/zulu/bin/zulu17.42.21-ca-crac-jdk17.0.7-linux_aarch64.tar.gz" ;;
+    *)       url="https://cdn.azul.com/zulu/bin/zulu17.42.21-ca-crac-jdk17.0.7-linux_x64.tar.gz" ;;
+esac
+
+echo "Using CRaC enabled JACK $url"
+
 ./mvnw clean package
-docker build -t sdeleuze/spring-boot-crac-demo:builder .
+docker build -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
 docker run -d --privileged --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-boot-crac-demo:builder
 echo "Please wait during creating the checkpoint..."
 sleep 10
