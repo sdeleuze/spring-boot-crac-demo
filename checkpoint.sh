4c580f75abc8f735177bdc62813078cc0082fb43
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-19 16:14:22 +0200

    Specify Linux capabilities

--- checkpoint.sh
+++ checkpoint.sh
@@ -10,7 +10,7 @@ echo "Using CRaC enabled JACK $url"
 
 ./mvnw clean package
 docker build -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
-docker run -d --privileged --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-boot-crac-demo:builder
+docker run -d --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --cap-add SYS_PTRACE --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-boot-crac-demo:builder
 echo "Please wait during creating the checkpoint..."
 sleep 10
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=spring-boot-crac-demo") sdeleuze/spring-boot-crac-demo:checkpoint
--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-docker run --rm -p 8080:8080 --name spring-boot-crac-demo sdeleuze/spring-boot-crac-demo:checkpoint
+docker run --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --rm -p 8080:8080 --name spring-boot-crac-demo sdeleuze/spring-boot-crac-demo:checkpoint
