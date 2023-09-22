5ab1a6ce2b2f02ca21ffa88ea4bc8b2b5dc41436
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-22 09:23:51 +0200

    Rollback to using --privileged for container creation
    Required to be able to restore with the specified PID

--- checkpoint.sh
+++ checkpoint.sh
@@ -6,11 +6,11 @@ case $(uname -m) in
     *)       url="https://cdn.azul.com/zulu/bin/zulu17.44.55-ca-crac-jdk17.0.8.1-linux_x64.tar.gz" ;;
 esac
 
-echo "Using CRaC enabled JACK $url"
+echo "Using CRaC enabled JDK $url"
 
 ./mvnw clean package
 docker build -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
-docker run -d --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --cap-add SYS_PTRACE --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-boot-crac-demo:builder
+docker run -d --privileged --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-boot-crac-demo:builder
 echo "Please wait during creating the checkpoint..."
 sleep 10
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=spring-boot-crac-demo") sdeleuze/spring-boot-crac-demo:checkpoint
