18eda0bff0aae721f6657c01835d08ad79e59959
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-09-11 15:46:52 +0200

    Upgrade to Java 21
    Allows to test virtual threads if needed.

--- checkpoint.sh
+++ checkpoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 case $(uname -m) in
-    arm64)   url="https://cdn.azul.com/zulu/bin/zulu17.44.55-ca-crac-jdk17.0.8.1-linux_aarch64.tar.gz" ;;
-    *)       url="https://cdn.azul.com/zulu/bin/zulu17.44.55-ca-crac-jdk17.0.8.1-linux_x64.tar.gz" ;;
+    arm64)   url="https://cdn.azul.com/zulu/bin/zulu21.36.19-ca-crac-jdk21.0.4-linux_aarch64.tar.gz" ;;
+    *)       url="https://cdn.azul.com/zulu/bin/zulu21.36.19-ca-crac-jdk21.0.4-linux_x64.tar.gz" ;;
 esac
 
 echo "Using CRaC enabled JDK $url"
 
 ./mvnw clean package
-docker build -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
+docker build --no-cache --progress=plain -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
 docker run -d --privileged --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt -e FLAG=$1 sdeleuze/spring-boot-crac-demo:builder
 echo "Please wait during creating the checkpoint..."
 sleep 10
